979d4f9606999bc3f9895f0e27240c2d8403220d
TheSnoStorm <thesnostorm@hotmail.com>
2014-09-08 04:55:30 -0500

    new classes

---
+++ src/rusty/wot/api/globalwar/FamePoints.java
@@ -0,0 +1,45 @@
+package rusty.wot.api.globalwar;
+
+import org.joda.time.DateTime;
+
+
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class FamePoints implements Comparable<FamePoints> {
+private String bbOutput = null;
+public String getBbOutput() {
+	return bbOutput;
+}
+public void setBbOutput(String bbOutput) {
+	this.bbOutput = bbOutput;
+}
+public Integer account_id;
+public Integer fame_points;
+public Integer position;
+private DateTime updated_at;
+
+@JsonProperty("updated_at")
+	public DateTime getUpdatedAt() {
+	return updated_at;
+}
+	@JsonProperty("updated_at")
+public void setUpdatedAt(long timeStamp) {
+	this.updated_at = new DateTime(timeStamp *1000l);
+}
+	public FamePoints() {
+	}
+@Override
+	public String toString() {
+	return bbOutput;
+	}
+@Override
+public int compareTo(FamePoints that) {
+	if(that == null) return 1;
+	else{
+		
+		return this.position.compareTo(that.position);
+	}
+}
+
+}
---
+++ src/rusty/wot/api/tanks/Stats.java
@@ -0,0 +1,42 @@
+package rusty.wot.api.tanks;
+
+public class Stats {
+		@Override
+	public String toString() {
+		return "Stats [account_id=" + account_id + ", mark_of_mastery=" + mark_of_mastery + ", max_frags=" + max_frags + ", max_xp=" + max_xp + ", tank_id=" + tank_id + ", in_garage=" + in_garage + ", all=" + all + ", clan=" + clan + ", company=" + company + "]";
+	}
+		public Integer account_id;
+		public Integer mark_of_mastery;
+		public Integer max_frags;
+		public Integer max_xp;
+		public Integer tank_id;
+		public Boolean in_garage;
+		public StatSet all;
+		public StatSet clan;
+		public StatSet company;
+	public Stats() {
+		super();
+	}
+	public static class StatSet {
+		@Override
+		public String toString() {
+			return "StatSet [battle_avg_xp=" + battle_avg_xp + ", battles=" + battles + ", capture_points=" + capture_points + ", damage_dealt=" + damage_dealt + ", damage_received=" + damage_received + ", draws=" + draws + ", dropped_capture_points=" + dropped_capture_points + ", frags=" + frags + ", hits=" + hits + ", hits_percents=" + hits_percents + ", losses=" + losses + ", shots=" + shots + ", spotted=" + spotted + ", survived_battles=" + survived_battles + ", wins=" + wins + ", xp=" + xp + "]";
+		}
+		public Integer battle_avg_xp;
+		public Integer battles;
+		public Integer capture_points;
+		public Integer damage_dealt;
+		public Integer damage_received;
+		public Integer draws;
+		public Integer dropped_capture_points;
+		public Integer frags;
+		public Integer hits;
+		public Integer hits_percents;
+		public Integer losses;
+		public Integer shots;
+		public Integer spotted;
+		public Integer survived_battles;
+		public Integer wins;
+		public Integer xp;
+	}
+}
